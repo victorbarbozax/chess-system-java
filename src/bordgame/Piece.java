1bf485ac7ca6c0e25cc69240439533c2a072d969
Victor Barboza <victor.barboza052008@gmail.com>
2025-10-28 21:54:19 -0300

    Possible moves of a piece

--- src/bordgame/Piece.java
+++ src/bordgame/Piece.java
@@ -1,6 +1,6 @@
 package bordgame;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position;
 	private Board board;
@@ -14,9 +14,23 @@ public class Piece {
 		return board;
 	}
 
+	public abstract boolean[][] possibleMoves();
 
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumm()];
+	}
 	
-	
+	public boolean isThereAnyPossibleMove() {
+		boolean [][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 	
 	
 	
